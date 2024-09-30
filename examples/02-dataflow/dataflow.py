246b06ce40d040e796860d325c830934848c271e
Lux <iansseijelly@berkeley.edu>
2024-09-29 22:28:36 -0700

    ADD: liveliness pass

--- examples/02-dataflow/dataflow.py
+++ examples/02-dataflow/dataflow.py
@@ -36,6 +36,18 @@ def intersect_const_maps(pred_const_maps: list[dict]):
     log_file.write(f"Intersected const map: {inter_const_map}\n")
     return inter_const_map
 
+def union_live_sets(succ_live_sets: list[set]):
+    """
+    Given a list of live sets, return the union of all the live sets.
+
+    Args:
+        succ_live_sets: a list of live sets
+
+    Returns:
+        the union of all the live sets
+    """
+    return set().union(*succ_live_sets)
+
 def foldable(insn: dict, block_const_map: dict):
     """
     Check if the instruction is foldable.
@@ -70,10 +82,10 @@ def calc_folded_value(insn: dict, block_const_map: dict):
         return None
 
 def const_prop_and_fold(cfg, entry_block):
-    const_map : dict[str, dict] = {}
+    const_map : dict[str, dict] = {} # a per-block const map
     # reverse the post order of the cfg
-    post_order_rev = list(reversed(list(nx.dfs_postorder_nodes(cfg, source=entry_block))))
-    worklist = post_order_rev
+    post_order = list(list(nx.dfs_postorder_nodes(cfg, source=entry_block)))
+    worklist = post_order
 
     while worklist:
         block = worklist.pop()
@@ -98,6 +110,32 @@ def const_prop_and_fold(cfg, entry_block):
             const_map[block.label] = block_const_map
             worklist.extend(cfg.successors(block))
 
+def live_variable_analysis(cfg, entry_block):
+    in_map : dict[str, set] = {} # a per-block live map (in)
+    out_map : dict[str, set] = {} # a per-block live map (out)
+    post_order = list(reversed(list(nx.dfs_postorder_nodes(cfg, source=entry_block))))
+    worklist = post_order
+
+    while worklist:
+        block = worklist.pop()
+        log_file.write(f"Processing block {block.label}\n")
+        successors = list(cfg.successors(block))
+        block_out_map = union_live_sets([in_map.get(succ.label, set()) for succ in successors])
+        for i, insn in enumerate(block.instrs):
+            if "dest" in insn:
+                if insn["dest"] in block_out_map:
+                    block_out_map.remove(insn["dest"])
+            for arg in insn.get("args", []):
+                if arg in in_map:
+                    block_out_map.add(arg)
+        if block.label not in in_map or in_map[block.label] != block_out_map:
+            in_map[block.label] = block_out_map
+            worklist.extend(cfg.predecessors(block))
+
+def print_live_map(live_map):
+    for block_label, live_set in live_map.items():
+        print(f"Block {block_label}: {live_set}")
+
 if __name__ == "__main__":
     log_file = open("log/dataflow.log", "w")
     prog = json.load(sys.stdin)
@@ -107,5 +145,6 @@ if __name__ == "__main__":
         program_cfg, entry_block = cfg.construct_cfg(fn["instrs"])
         print(program_cfg)
         const_prop_and_fold(program_cfg, entry_block)
+        live_variable_analysis(program_cfg, entry_block)
         fn["instrs"] = cfg.cfg_to_instrs(program_cfg)
     json.dump(prog, sys.stdout, indent=2)
