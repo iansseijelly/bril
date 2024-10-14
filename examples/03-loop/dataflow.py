7365c29d53f6d7e7aa463df785cc22bc91abb44f
Lux <iansseijelly@berkeley.edu>
2024-10-13 18:19:32 -0700

    ADD: working SSA

---
+++ examples/03-loop/dataflow.py
@@ -0,0 +1,190 @@
+import json
+import sys
+import cfg
+import networkx as nx
+from collections import OrderedDict
+
+def all_same(values: list):
+    """
+    Check if all the values in the list are the same.
+
+    Args:
+        values: a list of values
+
+    Returns:
+        True if all the values are the same, False otherwise
+    """
+    return len(set(values)) == 1
+
+def intersect_const_maps(pred_const_maps: list[dict]):
+    """
+    Given a list of const maps, return the intersection of all the const maps.
+
+    Args:
+        const_maps: a list of const maps
+
+    Returns:
+        inter_const_map: the intersection of all the const maps
+    """
+    all_keys = set().union(*pred_const_maps)
+    inter_const_map = {}
+    for key in all_keys:
+        values = [pred_const_map.get(key, None) for pred_const_map in pred_const_maps]
+        # if all the values are the same, then we can set the value to that
+        if all_same(values):
+            inter_const_map[key] = values[0]
+    log_file.write(f"Intersected const map: {inter_const_map}\n")
+    return inter_const_map
+
+def union_live_sets(succ_live_sets: list[set]):
+    """
+    Given a list of live sets, return the union of all the live sets.
+
+    Args:
+        succ_live_sets: a list of live sets
+
+    Returns:
+        the union of all the live sets
+    """
+    return set().union(*succ_live_sets)
+
+def foldable(insn: dict, block_const_map: dict):
+    """
+    Check if the instruction is foldable.
+
+    Args:
+        insn: an instruction
+
+    Returns:
+        True if the instruction is foldable, False otherwise
+    """
+    foldable_ops = ["add", "sub", "mul"]
+    return insn["op"] in foldable_ops and insn["args"][0] in block_const_map and insn["args"][1] in block_const_map
+
+def calc_folded_value(insn: dict, block_const_map: dict):
+    """
+    Calculate the folded value of the instruction.
+
+    Args:
+        insn: an instruction
+        block_const_map: a const map of the block
+
+    Returns:
+        the folded value of the instruction
+    """
+    if insn["op"] == "add":
+        return block_const_map[insn["args"][0]] + block_const_map[insn["args"][1]]
+    elif insn["op"] == "sub":
+        return block_const_map[insn["args"][0]] - block_const_map[insn["args"][1]]
+    elif insn["op"] == "mul":
+        return block_const_map[insn["args"][0]] * block_const_map[insn["args"][1]]
+    else:
+        return None
+
+def const_prop_and_fold(cfg, entry_block):
+    const_map : dict[str, dict] = {} # a per-block const map
+    # reverse the post order of the cfg
+    post_order = list(list(nx.dfs_postorder_nodes(cfg, source=entry_block)))
+    worklist = post_order
+
+    while worklist:
+        block = worklist.pop()
+        log_file.write(f"Processing block {block.label}\n")
+        predecessors = list(cfg.predecessors(block))
+        # get the intersection of all predecessors' const maps
+        inter_const_map = intersect_const_maps([const_map.get(pred.label, {}) for pred in predecessors])
+        block_const_map = inter_const_map
+        for i, insn in enumerate(block.instrs):
+            if "op" in insn:
+                if insn["op"] == "const":
+                    block_const_map[insn["dest"]] = insn["value"]
+                elif foldable(insn, block_const_map):
+                    log_file.write(f"folding {insn['op']} for {insn}\n")
+                    block.instrs[i] = {
+                        "op": "const",
+                        "dest": insn["dest"],
+                        "value": calc_folded_value(insn, block_const_map),
+                        "type": insn["type"]
+                    }
+                    block_const_map[insn["dest"]] = block.instrs[i]["value"]
+        if block.label not in const_map or const_map[block.label] != block_const_map:
+            const_map[block.label] = block_const_map
+            worklist.extend(cfg.successors(block))
+
+def should_keep(instr):
+    if "op" not in instr:
+        return True
+    return instr["op"] != "nop"
+
+def remove_nops(instrs):
+    return [instr for instr in instrs if should_keep(instr)]
+
+def used_locally(block, target_insn):
+    """
+    Check if the destination of this instruction is used locally LATER.
+
+    Args:
+        block: the block containing the instruction
+        insn: the instruction
+
+    Returns:
+        True if the destination is used locally, False otherwise
+    """
+    found = False
+    for i, curr_insn in enumerate(block.instrs):
+        if curr_insn == target_insn:
+            found = True
+        if found and "args" in curr_insn and target_insn["dest"] in curr_insn["args"]:
+            return True
+    return False
+
+def live_variable_analysis(cfg, entry_block):
+    in_map : dict[str, set] = {} # a per-block live map (in)
+    out_map : dict[str, set] = {} # a per-block live map (out)
+    post_order = list(reversed(list(nx.dfs_postorder_nodes(cfg, source=entry_block))))
+    worklist = post_order
+
+    while worklist:
+        block = worklist.pop()
+        log_file.write(f"Processing block {block.label}\n")
+        successors = list(cfg.successors(block))
+        log_file.write(f"Successors labels: {[succ.label for succ in successors]}\n")
+        block_out_map = union_live_sets([in_map.get(succ.label, set()) for succ in successors])
+        log_file.write(f"block_out_map: {block_out_map}\n")
+        if block.label not in out_map or out_map[block.label] != block_out_map:
+            out_map[block.label] = block_out_map
+            log_file.write(f"Updating out_map for block {block.label}\n")
+        log_file.write(f"out_map: {out_map}\n")
+        block_in_map = block_out_map.copy()
+        for i, insn in reversed(list(enumerate(block.instrs))):
+            if "dest" in insn:
+                if insn["dest"] in block_in_map:
+                    block_in_map.remove(insn["dest"])
+            for arg in insn.get("args", []):
+                block_in_map.add(arg)
+        if block.label not in in_map or in_map[block.label] != block_in_map:
+            in_map[block.label] = block_in_map
+            log_file.write(f"Updating in_map for block {block.label}\n")
+            log_file.write(f"in_map: {in_map}\n")
+            worklist.extend(cfg.predecessors(block))
+    
+    for block in cfg.nodes:
+        for i, insn in reversed(list(enumerate(block.instrs))):
+            if "dest" in insn and insn["dest"] not in out_map[block.label] and not used_locally(block, insn):
+                log_file.write(f"Marking {insn} as dead for block {block.label}\n")
+                block.instrs[i]["op"] = "nop"
+    
+    for block in cfg.nodes:
+        block.instrs = remove_nops(block.instrs)
+
+if __name__ == "__main__":
+    log_file = open("log/dataflow.log", "w")
+    prog = json.load(sys.stdin)
+    log_file.write(f"Processing {len(prog['functions'])} function(s)\n")
+    for fn in prog["functions"]:
+        log_file.write(f"--> Processing function {fn['name']} -->\n")
+        program_cfg, entry_block = cfg.construct_cfg(fn["instrs"])
+        const_prop_and_fold(program_cfg, entry_block)
+        live_variable_analysis(program_cfg, entry_block)
+        fn["instrs"] = cfg.cfg_to_instrs(program_cfg)
+    json.dump(prog, sys.stdout, indent=2)
---
+++ examples/03-loop/ssa.py
@@ -0,0 +1,245 @@
+import json
+import sys
+import cfg
+import networkx as nx
+from collections import defaultdict
+
+def get_def_blocks(cfg: nx.DiGraph):
+    """
+    Get a map from variable names to defining blocks.
+    """
+    log_file.write("--- Getting def blocks ---")
+    out = defaultdict(set)
+    for block in cfg.nodes:
+        for instr in block.instrs:
+            if 'dest' in instr:
+                out[instr['dest']].add(block)
+    for var in out:
+        log_file.write(f"{var}: {[b.label for b in out[var]]}\n")
+    return out
+
+def intersect(sets):
+    """
+    Intersect a list of sets.
+    """
+    sets = list(sets)
+    if len(sets) == 0:
+        return set()
+    return set.intersection(*sets)
+
+def get_dominators(cfg: nx.DiGraph, entry: cfg.BasicBlock):
+    """
+    Get all dominators of a given CFG.
+    """
+    log_file.write("--- Getting dominators ---")
+    post_order = list(reversed(list(nx.dfs_postorder_nodes(cfg, source=entry))))
+    all_nodes = set(cfg.nodes)
+    dom = {v: all_nodes for v in cfg.nodes}
+    log_file.write(f"Initial dom:")
+    for block in dom:
+        log_file.write(f"{block.label}: {[b.label for b in dom[block]]}\n")
+    while True:
+        changed = False
+        for node in post_order:
+            preds = list(cfg.predecessors(node))
+            new_dom = intersect(dom[p] for p in preds)
+            new_dom.add(node)
+            if dom[node] != new_dom:
+                dom[node] = new_dom
+                changed = True
+        if not changed:
+            break
+    for block in dom:
+        log_file.write(f"{block.label}: {[b.label for b in dom[block]]}\n")
+    return dom
+
+def rev_dom(dom: dict[cfg.BasicBlock, set[cfg.BasicBlock]]):
+    """
+    Given who is dominated by whom, return who dominates whom.
+    """
+    out = defaultdict(set)
+    for block in dom:
+        for dominated in dom[block]:
+            out[dominated].add(block)
+    return out
+
+def get_dominance_frontier(cfg: nx.DiGraph, dom: dict[cfg.BasicBlock, set[cfg.BasicBlock]]):
+    """
+    Get the dominance frontier, given the dominance relation.
+    """
+    log_file.write("--- Getting dominance frontier ---")
+    frontier = defaultdict(set)
+    rev_dom_map = rev_dom(dom)
+    for block in rev_dom_map:
+        dominated_succs = set()
+        for dominated in rev_dom_map[block]:
+            dominated_succs.update(cfg.successors(dominated))
+        frontier[block] = [b for b in dominated_succs if b not in rev_dom_map[block] or b == block]
+    for block in frontier:
+        log_file.write(f"{block.label}: {[b.label for b in frontier[block]]}\n")
+    return frontier
+
+def get_dom_tree(dom: dict[cfg.BasicBlock, set[cfg.BasicBlock]]):
+    """
+    Get the dominance tree, given the dominance relation.
+    """
+    log_file.write("--- Getting dominance tree ---")
+    rev_dom_map = rev_dom(dom)
+    dom_rev_strict = {a: {b for b in bs if b != a} for a, bs in rev_dom_map.items()} # dominated but not self
+    # grandparent domination
+    dom_rev_strict_2x = {a: set().union(*(dom_rev_strict[b] for b in bs)) for a, bs in dom_rev_strict.items()} 
+    return {a: {b for b in bs if b not in dom_rev_strict_2x[a]} for a, bs in dom_rev_strict.items()}
+
+def get_block_defs(blocks):
+    """
+    Get a map from variable names to defining blocks.
+    """
+    out = defaultdict(set)
+    for name, block in blocks.items():
+        for instr in block:
+            if 'dest' in instr:
+                out[instr['dest']].add(name)
+    return dict(out)
+
+def get_phis(cfg, df, defs):
+    """
+    Find where to insert phi-nodes in the blocks.
+    """
+    log_file.write(" --- Getting phis --- ")
+    phis = {b: set() for b in cfg.nodes}
+    for v, v_defs in defs.items():
+        v_defs_list = list(v_defs)
+        # print(f"Defining {v} in {[b for b in v_defs_list]}")
+        for d in v_defs_list:
+            for block in df[d]:
+                if v not in phis[block]:
+                    phis[block].add(v)
+                    # print(f"Adding phi for {v} in {block.label}")
+                    if block not in v_defs_list:
+                        v_defs_list.append(block)
+    for block in phis:
+        log_file.write(f"{block.label}: {[v for v in phis[block]]}\n")
+    return phis
+
+def get_types(func):
+    """
+    Get the types of the variables in a function.
+    """
+    types = {arg['name']: arg['type'] for arg in func.get('args', [])}
+    for instr in func['instrs']:
+        if 'dest' in instr:
+            types[instr['dest']] = instr['type']
+    return types
+
+def get_arg_names(func):
+    """
+    Get the names of the arguments to a function.
+    """
+    return {arg['name'] for arg in func.get('args', [])}
+
+def ssa_rename(cfg, phis, dom_tree, arg_names, entry):
+    stack = defaultdict(list, {v: [v] for v in arg_names})
+    phi_args = {b: {p: [] for p in phis[b]} for b in cfg.nodes} # dict of dicts of lists
+    phi_dests = {b: {p: None for p in phis[b]} for b in cfg.nodes} # dict of dicts of lists
+    counters = defaultdict(int) # assign the next usable name per variable
+
+    def _push_fresh(var):
+        fresh = '{}.{}'.format(var, counters.get(var, 0))
+        counters[var] += 1
+        stack[var].insert(0, fresh)
+        return fresh
+
+    def _rename(block):
+        log_file.write(f"Renaming block {block.label}\n")
+        # Save stacks.
+        old_stack = {k: list(v) for k, v in stack.items()}
+
+        # Rename phi-node destinations.
+        for p in phis[block]:
+            phi_dests[block][p] = _push_fresh(p)
+
+        for instr in block.instrs:
+            log_file.write(f"Renaming {instr}\n")
+            # Rename arguments in normal instructions.
+            if 'args' in instr:
+                new_args = [stack[arg][0] for arg in instr['args']]
+                instr['args'] = new_args
+                log_file.write(f"Renaming args for {instr['args']} in {block.label}\n")
+            # Rename destinations.
+            if 'dest' in instr:
+                instr['dest'] = _push_fresh(instr['dest'])
+                log_file.write(f"Renaming dest for {instr['dest']} in {block.label}\n")
+        # Rename phi-node arguments (in successors).
+        for s in cfg.successors(block):
+            log_file.write(f"Renaming successors of {block.label}: {s.label}\n")
+            for p in phis[s]:
+                if stack.get(p):
+                    phi_args[s][p].append((block, stack[p][0]))
+                else:
+                    # The variable is not defined on this path
+                    phi_args[s][p].append((block, "__undefined"))
+
+        # Recursive calls.
+        for b in sorted(dom_tree[block]):
+            _rename(b)
+
+        # Restore stacks.
+        stack.clear()
+        stack.update(old_stack)
+
+    _rename(entry)
+
+    return phi_args, phi_dests
+
+def insert_phis(cfg, phi_args, phi_dests, types):
+    for block in cfg.nodes:
+        if len(block.instrs) > 0:
+            new_instrs = [block.instrs[0]]
+            for dest, pairs in sorted(phi_args[block].items()):
+                phi = {
+                    'op': 'phi',
+                    'dest': phi_dests[block][dest],
+                    'type': types[dest],
+                    'labels': [p[0].label for p in pairs],
+                    'args': [p[1] for p in pairs],
+                }
+                new_instrs.append(phi)
+                log_file.write(f"Inserting phi for {dest} in {block.label}\n")
+            # Append the original instructions after the phi nodes
+            new_instrs.extend(block.instrs[1:])
+            # print(block.instrs)
+            # print(new_instrs)
+            # Replace the block's instructions with the new list
+            block.instrs = new_instrs
+            # print(block.instrs)
+    
+    # for block in cfg.nodes:
+        # print(block.instrs)
+
+if __name__ == "__main__":
+    prog = json.load(sys.stdin)
+    log_file = open("log/ssa.log", "w")
+    for fn in prog["functions"]:
+        program_cfg, entry_block = cfg.construct_cfg(fn["instrs"])
+        def_blocks = get_def_blocks(program_cfg)
+        dom = get_dominators(program_cfg, entry_block)
+        rev_dom_map = rev_dom(dom)
+        log_file.write("Reverse dominance:\n")
+        for block in rev_dom_map:
+            log_file.write(f"{block.label}: {[b.label for b in rev_dom_map[block]]}\n")
+        df = get_dominance_frontier(program_cfg, dom)
+        phis = get_phis(program_cfg, df, def_blocks)
+        types = get_types(fn)
+        arg_names = get_arg_names(fn)
+        dom_tree = get_dom_tree(dom)
+        log_file.write("Dominance tree:\n")
+        for block in dom_tree:
+            log_file.write(f"{block.label}: {[b.label for b in dom_tree[block]]}\n")
+        phi_args, phi_dests = ssa_rename(program_cfg, phis, dom_tree, arg_names, entry_block)
+        insert_phis(program_cfg, phi_args, phi_dests, types)
+        log_file.write("--- Program after SSA ---\n")
+        for block in program_cfg.nodes:
+            log_file.write(f"{block.label}: {block.instrs}\n")
+        fn["instrs"] = cfg.cfg_to_instrs(program_cfg)
+    json.dump(prog, sys.stdout, indent=2)
+    log_file.close()
